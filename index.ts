c02a42cd19dabaac91191d63738ede9ba09e50f2
tomasquintana-gj <tomquintana20@gmail.com>
2023-06-07 16:04:59 -0300

    set swagger for documentation

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express  from 'express';
 import morgan from 'morgan';
+import swaggerJsonDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import routesMeal from './src/routes/meals.routes';
+import { options } from './src/docs/swaggerOptions';
 
 const app = express();
+
+const spect = swaggerJsonDoc(options);
 
 app.use(morgan('dev'));
 
 app.use('/api/meal', routesMeal);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(spect));
 
 app.listen(4000, () => {
   console.log('Servidor corriendo en el puerto 4000');
--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -4,5 +4,13 @@ import { test } from '../controllers/meal.controller';
 const router = express.Router();
 
 router.get('/register', test);
-
+/**
+   * @swagger
+   * /register:
+   *   get:
+   *     description: Register a new meals
+   *     responses:
+   *       200:
+   *         description: hello world
+   */
 export default router;
